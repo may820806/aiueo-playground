4a6933c5935a344faa9773f3bbfb2b6ee00f23f7
MayWeng <may820806@gmail.com>
2024-04-11 16:09:30 +0800

    feat: add 'enter' event listener

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
+const intercept = require('intercept-stdout');
+
 const nextConfig = {
   reactStrictMode: true,
 }
+
+// safely ignore recoil warning messages in dev (triggered by HMR)
+function interceptStdout(text) {
+  if (text.includes('Duplicate atom key')) {
+    return "";
+  }
+  return text;
+}
+
+intercept(interceptStdout);
 
 module.exports = nextConfig
